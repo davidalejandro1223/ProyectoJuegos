403952079eacac99c74c0c1aefb3e4439686250e
David Aparicio <davidalejandro1223@hotmail.com>
2017-11-10 19:49:31 -0500

    commit inicial

---
+++ Juegos/usuario/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect, loader
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.http.response import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def autenticar(request):
+    if request.method == 'POST':
+        usuario = request.POST.get('inputUsuario', None)
+        contrasena = request.POST.get('inputContrasena', None)
+
+        user = authenticate(username=usuario, password=contrasena)
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            return HttpResponse('usuario no existe')
+
+    return render(request, 'login.html', {})
